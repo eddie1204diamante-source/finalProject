d9a6ee0518a4940b43ca5bc94ee1854f89ec1ac4
Eddie Vargas <eddie1204diamante@gmail.com>
2026-06-16 23:02:17 -0500

    Configurar MySQL Railway

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("MYSQL_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
+
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 
